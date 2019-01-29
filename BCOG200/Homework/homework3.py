1006622ca803f76a7547baefa4a03ed4bbe53abd
jonwillits <jontest003@gmail.com>
2019-01-29 15:32:05 -0600

    updated homework

--- BCOG200/Homework/homework3.py
+++ BCOG200/Homework/homework3.py
@@ -8,12 +8,59 @@
     2. how do you only read the first 10 characters of a file? type your answer as working python code in the space
         indicated below.    
 '''
-print("Output of question #2")
-# your code for #2 goes here
+# type your answer here
+
+''' 
+    3. Describe the difference behavior between the two following lines of code:
+      if not (x > 3) or (y > 3):
+      if not ((x > 3) or (y > 3)):  
+'''
+# type your answer here
+
+''' 
+    4.  Can you use 'break' and 'continue' with for loops, or just with while loops?
+'''
+# type your answer here
+
+
+''' 
+   5. if you want to remove the 3rd item from a list, what is the one line of python code to do that
+'''
+# type your answer here
+
+
+''' 
+    6. Write a program that asks for the temperature, and asks if it is in C or F, and converts it to the other 
+        and prints it out.
+'''
+print("Output of question #6")
+# your code for #6 goes here
 print()
 
+
+''' 
+    7. Write a program that will compute the area of a circle. 
+    Prompt the user to enter the radius and print a nice message back to the user with the answer.
 '''
-    3.  write code that reads in file 'test_file2.txt', creates a separate file for each geographic region 
+print("Output of question #7")
+# your code for #7 goes here
+print()
+
+
+''' 
+    8.  It is possible to name the days 0 through 6 where day 0 is Sunday and day 6 is Saturday.
+        If you go on a wonderful holiday leaving on day number 3 (a Wednesday) and you return home after 10 nights. 
+    Write a general version of the program which asks for the starting day number, and the length of your stay, 
+    and it will tell you the number of day of the week you will return on.
+'''
+print("Output of question #8")
+# your code for #8 goes here
+print()
+
+
+
+'''
+    9.  write code that reads in file 'test_file2.txt', creates a separate file for each geographic region 
         the animals are from, and prints only the animals from that region to the file, one animal per line.
         for example, you should create the file 'africa.txt', and that file should have:
         
@@ -27,14 +74,26 @@ print()
         files from the input files. For example, if I test your code on a new input file that has animals from china, or
         antarctica, or Wonderland, the program should still work!
 '''
-print("Output of question #3")
-# your code for #3 goes here
+print("Output of question #9")
+# your code for #9 goes here
 print()
 
-'''
-    4.  write code where you enter numbers using the input 
+
 
 '''
-print("Output of question #4")
-# your code for #4 goes here
+    10. write code that:
+        1.) takes 'test_file1.txt' as input
+        2.) stores each line in list
+        3.) uses a for loop to iterate over that list
+        4.) uses a second for loop to iterate over each word in each sentence
+        5.) uses a while loop to iterate over each letter in the word, until there are no more letters in the word
+        6.) prints out the document, one letter at a time, with each word on a different line and each letter separated
+                by a space. For example, the first three lines should be:
+                
+                T h e
+                g r i z z l y
+                b e a r
+'''
+print("Output of question #10")
+# your code for #10 goes here
 print()
