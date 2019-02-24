e95846059b83f69b8dfbe4e1ba2611748a0274b3
jonwillits <jontest003@gmail.com>
2019-02-24 16:26:28 -0600

    added lab 7

---
+++ BCOG200/Labs/7/analyze_lyrics.py
@@ -0,0 +1,96 @@
+import sys
+import os
+
+# STEP 1: Comment the main() function
+# STEP 2: Complete the code for the eight class methods below that have no code.
+#           Note: in this program we are keeping track of types and token separately for each song, and then also
+#           creating a type and token summary number for the artist that combines all of their songs together.
+#           Feel free to refer back to our code from earlier weeks. But you will probably need to adapt it slightly
+#           If you want to test yourself, coding it from scratch again is good practice.
+#           HINT: you dont need to count freqs, types, and tokens twice.
+#           Do it for the songs
+#           Then, combine the freq dictionaries for each song to create one summary freq dictionary for the artist
+#           Then you can count the artist's overall types and tokens from that summary dictionary
+
+class Artist:
+
+    def __init__(self, name, directory_location):
+        self.name = name
+        self.directory_location = directory_location
+        self.num_songs = 0
+        self.song_list = []
+        self.lyric_freq_dict = {}
+        self.num_tokens = 0
+        self.num_types = 0
+
+    def get_freq_dict(self):
+        pass
+
+    def get_word_type_sum(self):
+        pass
+
+    def get_word_token_sum(self):
+        pass
+
+
+class Song:
+
+    def __init__(self, title, file_location):
+        self.title = title
+        self.file_location = file_location
+        self.lyric_list = []
+        self.lyric_freq_dict = {}
+        self.num_tokens = 0
+        self.num_types = 0
+
+    def get_lyrics(self):
+        pass
+
+    def get_freq_dict(self):
+        pass
+
+    def get_word_type_sum(self):
+        pass
+
+    def get_word_token_sum(self):
+        pass
+
+
+def remove_hidden_files(input_list):
+    output_list = []
+    for item in input_list:
+        if item[0] != '.':
+            output_list.append(item)
+    return output_list
+
+
+def main():
+    artist_object_list = []
+    input_directory = sys.argv[1]
+    artist_directory_list = os.listdir(input_directory)
+    artist_list = remove_hidden_files(artist_directory_list)
+
+    for artist_name in artist_list:
+
+        artist_directory = input_directory+artist_name
+        new_artist_instance = Artist(artist_name, artist_directory)
+        song_directory_list = os.listdir(artist_directory)
+        song_filename_list = remove_hidden_files(song_directory_list)
+
+        for song_filename in song_filename_list:
+            song_title = song_filename[:-4]
+            song_location = artist_directory + "/" + song_title
+            new_song_instance = Song(song_title, song_location)
+            new_artist_instance.song_list.append(new_song_instance)
+
+        artist_object_list.append(new_artist_instance)
+
+    for i in range(len(artist_object_list)):
+        artist = artist_object_list[i]
+        print("{}     Types: {}      Tokens: {}".format(artist.name, artist.num_types, artist.num_tokens))
+        for j in range(artist.num_songs):
+            song = artist.song_list[j]
+            print("     {}      Types: {}      Tokens: {}".format(song.title, song.num_types, song.num_tokens))
+
+
+main()
---
+++ BCOG200/Labs/7/classes.py
@@ -0,0 +1,61 @@
+""" We're on the homestretch for basic python. The last thing we need to learn about are classes. Classes are complex data
+types that allow us to combine functions and data. You've already been using classes without knowing it. Strings, lists,
+sets, and dictionaries - even floats and ints -  are all examples of classes. They are classes because they have
+information stored in variables, and functions that are "a part of them" that allow you to do stuff to that information.
+
+For example, the list A = [1, 2, 3] effectively have four pieces of information, slot 1, slot 2, slot 3, and the list
+itself. It also has a bunch of functions that change the list. A.append(), A.sort(), A.pop(), and many others. Note
+the difference between two possible ways we might imagine writing the code for appending to a list:"""
+
+A = []  # creating an instance of the list class, called A
+A.append(4) # calling a method of the list class
+# A = append(A, 4), the way we would add a member to the list if python were a functional language
+
+"""In the first option (and the correct way to do this in python), we can tell that append() is a function that is a part
+of the list itself, because of the way that we access it using the list's name followed by a period. The second option,
+which does not work and is illegal in python, would be the way we would write it if append() were an independent
+function, that got passed a list and a new item as an argument, and returned us the list with that item appended.
+
+These two ways of doing it are two very different styles of languages, called "object-oriented" languages in the case
+of the python way, and called "functional" languages in the case of the other way. The difference comes down to whether
+the language, at its core, "thinks" in terms of objects, or "thinks" in terms of functions. Python is an object-oriented
+language because objects (like strings, lists, dictionaries, etc.) are the primitive building blocks of the language,
+and functions are parts of those building blocks, rather than the other way around.
+
+In addition to built-in classes like lists, we can create our own classes when we need to make complex data structures
+of our own that will have their own behaviors.
+
+Here you can see how we define a class."""
+
+class human:
+    pass
+
+"""Classes need to have something "inside" them, code that is tabbed or spaced over once (just like if statements, for
+loops, and functions). It is illegal to have a class definition without at least one thing inside it. Here, we are just 
+using the "pass" command  (which doesnt do anything), as the only thing inside the human class, since we have to have 
+something."""
+
+"""Next, see how we create a member of our human class."""
+my_human = human()
+
+"""Note how creating a member is very similar to how we create instances of built in classes."""
+my_float = float(5)
+my_set = set('doggy')
+
+""" See, just like we would create a float (from an int), or create a set (except of course that we pass arguments
+into the float or set class). Sets and floats appear in a special color because they are built in class types, whereas 
+our human class type is only defined in our program. We can see another example a difference between built-in classes 
+and user-defined classes if we print them."""
+print(my_float)
+print(my_set)
+print(my_human, '\n')
+
+"""The set shows you the actual data contents of a set, whereas human just gives you some machine gobbleygook that 
+includes the class name and also the memory location where that instance of the class is stored in memory. If you want
+your class to show actual information about the class, you can add that functionality, which we will show later (when
+our class has information to show."""
+
+# define your own class below.
+# Then write a loop that creates 10 instances of the class stored in a list.
+# Then write another loop that goes through the list and prints each one.
+
