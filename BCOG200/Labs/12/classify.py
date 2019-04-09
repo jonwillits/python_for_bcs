df714fb2200662959891537415f7aed27e6e9527
jonwillits <jontest003@gmail.com>
2019-04-09 15:10:33 -0500

    lab 9 and homework 9

--- BCOG200/Labs/12/classify.py
+++ BCOG200/Labs/12/classify.py
@@ -11,12 +11,12 @@ SVD_DIM = 0
 
 def main():
     my_data = dl.Dataset('data/data2.csv', TRAINING_PROPORTION, NORMALIZE, SVD_DIM, VERBOSE)
-    #my_data.plot_data()
-    #plt.show()
+    my_data.plot_data()
+    plt.show()
 
-    #my_knn = knn.Knn(my_data, MIN_MAX_KNN, VERBOSE)
-    #my_knn.train_model()
-    #my_knn.test_model(my_data.test_list, my_data.training_list, my_knn.best_k)
+    my_knn = knn.Knn(my_data, MIN_MAX_KNN, VERBOSE)
+    my_knn.train_model()
+    my_knn.test_model(my_data.test_list, my_data.training_list, my_knn.best_k)
 
     my_data.compute_feature_correlations()
 
