bedb62f7f095aaa6ed26b5da0ea806239cc727e4
jwillits@illinois.edu <Bimsawa.1>
2020-02-27 10:48:23 -0600

    updated week 8

--- Assignments/08/homework_08.py
+++ Assignments/08/homework_08.py
@@ -1,60 +1,54 @@
 
 def q1():
-    print("Question 00:"
-          "Explain the behavior of Vehicle 00. "
+    print("Question 1:"
+          "Explain the behavior of Vehicle 1. "
           "What is an example of a way that Braitenberg says that vehicle one could behave in a way that makes "
           "it look more complex than it really is? "
           "Can you think of other examples?")
-    #
     your_answer = ""
     print(your_answer)
 
 
 def q2():
-    print("Question 01: "
-          "The varieties of Vehicle 01 behave in ways that Braitenberg says remind of us of a more complex "
+    print("Question 2: "
+          "The varieties of Vehicle 1 behave in ways that Braitenberg says remind of us of a more complex "
           "organism demonstrating fear and aggression. Explain why this is so? "
           "Think of another example of human behavior where something complex is in fact possibly explainable "
           "by something much simpler, and give an example of the hypothetically simpler explanation."
           )
-    #
     your_answer = ""
     print(your_answer)
 
 
 def q3():
-    print("Question 02: "
-          "Vehicle 02 discusses the nature of knowledge. What does Braitenberg say about what knowledge is in this "
+    print("Question 3: "
+          "Vehicle 2 discusses the nature of knowledge. What does Braitenberg say about what knowledge is in this "
           "chapter? Do you agree with Braitenberg? Why or why not?")
-    #
     your_answer = ""
     print(your_answer)
 
 
 def q4():
-    print("""Question 03:
-          Complete Vehicle3.py as described in Lab. Make sure you have implemented the following:
-            - Add at least 00 more class representing another kind of input other than heat.
+    print("""Question 4:
+          Complete Vehicle3.py as described in lab_08_03.py. Make sure you have implemented the following:
+            - Add at least 1 more class representing another kind of input other than heat.
             - Have the input_list contain multiple kinds of input sources.
             - Use custom images for each input sources
-            - Modify vehicle 02 so that it has an 'innate preference' or 'innate dislike' of each kind of input""")
-
+            - Modify vehicle 3 so that it has an 'innate preference' or 'innate dislike' of each kind of input""")
     your_answer = ''
     print(your_answer)
 
 
 def q5():
-    print("""Question 05:
+    print("""Question 5:
     Create Vehicle4.py by saving a copy of your Vehicle3.py file and saving it as Vehicle4.py.
     Modify the vehicle so that, for things that it likes, it has a 'u-shaped' preference curve, moving little
     when it is far away from something, or when it is really close, but moving quickly when it is a moderate distance
     away. If you get this working correctly, you should get your Vehicles approaching and staying near the things they
     like.
     """)
-    #
     your_answer = ""
     print(your_answer)
-
 
 
 def main():
@@ -63,5 +57,6 @@ def main():
     q3()
     q4()
     q5()
+
 
 main()
